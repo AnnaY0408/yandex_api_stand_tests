508d98860a3c87ad0a4fe0697bba1c29d49c776f
ykupo@AnnaY <ykupovaanna@yandex.ru>
2024-01-19 18:45:38 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = 'https://74982f8d-1dcd-49e2-8c9e-ee8803d415e3.serverhub.praktikum-services.ru'
+USERS_TABLE_PATH = '/api/db/resources/user_model.csv'
+CREATE_USER_PATH = '/api/v1/users/'
+
